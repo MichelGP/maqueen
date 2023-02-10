f34d0eccd4dcb1facace1ca95e331f049fb6bd13
Michel van der Zijden <97062880+MichelGP@users.noreply.github.com>
2023-02-10 20:09:02 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 basic.forever(function () {
     if (DFRobotMaqueenPlus.ultraSonic(PIN.P13, PIN.P14) > 30) {
+        basic.showIcon(IconNames.Happy)
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 70)
     } else {
+        basic.showIcon(IconNames.Asleep)
         DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 70)
         DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 70)
         basic.pause(1500)
