0ae5902ce131b3e22444b7b196cd44697e2ae239
Michel van der Zijden <97062880+MichelGP@users.noreply.github.com>
2023-02-10 19:42:32 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 let Dist = 0
+let cur_heading = 0
 basic.forever(function () {
-    while (Dist > 50) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 78)
+    Dist = DFRobotMaqueenPlus.ultraSonic(PIN.P13, PIN.P14)
+    if (Dist > 30) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 70)
         Dist = DFRobotMaqueenPlus.ultraSonic(PIN.P13, PIN.P14)
+    } else {
+        cur_heading = input.compassHeading()
+        while (false) {
+            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 30)
+            if (cur_heading - input.compassHeading() > 90 || cur_heading - input.compassHeading() < 90) {
+                break;
+            }
+        }
     }
-    DFRobotMaqueenPlus.mototStop(Motors.ALL)
 })
