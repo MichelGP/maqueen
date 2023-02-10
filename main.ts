173841bf979be5ab0a39f7bbb77c82fa7136e8c0
Michel van der Zijden <97062880+MichelGP@users.noreply.github.com>
2023-02-10 19:15:59 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,8 @@
 let Dist = 0
-let Count = 0
 basic.forever(function () {
-    Dist = DFRobotMaqueenPlus.ultraSonic(PIN.P13, PIN.P14)
-    if (Dist > 50) {
-        while (Count < 4) {
-            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 78)
-            Count = parseInt(DFRobotMaqueenPlus.readeDistance(Motors1.M1))
-        }
-    } else {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 27)
-        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 90)
+    while (Dist > 50) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 78)
+        Dist = DFRobotMaqueenPlus.ultraSonic(PIN.P13, PIN.P14)
     }
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
 })
