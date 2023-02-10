46a0c7480b23e0ab660f2ea2e27f8209c8c8db9c
Michel van der Zijden <97062880+MichelGP@users.noreply.github.com>
2023-02-10 20:02:23 +0100

    Werkende obstacle avoiding rover

--- main.ts
+++ main.ts
@@ -1,17 +1,9 @@
-let Dist = 0
-let cur_heading = 0
 basic.forever(function () {
-    Dist = DFRobotMaqueenPlus.ultraSonic(PIN.P13, PIN.P14)
-    if (Dist > 30) {
+    if (DFRobotMaqueenPlus.ultraSonic(PIN.P13, PIN.P14) > 30) {
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 70)
-        Dist = DFRobotMaqueenPlus.ultraSonic(PIN.P13, PIN.P14)
     } else {
-        cur_heading = input.compassHeading()
-        while (false) {
-            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 30)
-            if (cur_heading - input.compassHeading() > 90 || cur_heading - input.compassHeading() < 90) {
-                break;
-            }
-        }
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 70)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 70)
+        basic.pause(1500)
     }
 })
